f8fca0d49d8f5e8ffd3229a17f002e8971eb5952
Nguyen Truong Toan To <toantnt76@gmail.com>
2025-09-07 21:56:58 +0700

    Add src folder with initial core scripts

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# This file makes the 'src' directory a Python package.
+
+# Optional: Define a version for your package.
+# This is a good practice for project management and reproducibility.
+__version__ = "0.1.0"
